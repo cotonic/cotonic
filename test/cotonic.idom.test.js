53fedae82e625ad9509994a147b3328ceedfc35c
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2017-01-28 11:33:29 +0100

    Added the possibility to pass tokens to the patch operation

--- test/cotonic.idom.test.js
+++ test/cotonic.idom.test.js
@@ -6,8 +6,8 @@
 
 var idom = cotonic.idom;
 
-QUnit.test("Idom patch", function(assert) {
-    var element = document.getElementById("test");
+QUnit.test("Idom patch html", function(assert) {
+    var element = document.getElementById("test-1");
     idom.patchInner(element, "<p>Hello World!</p>");
 
     var p = element .getElementsByTagName("p");
@@ -19,6 +19,41 @@ QUnit.test("Idom patch", function(assert) {
 
     /* Adding an element */
     idom.patchInner(element, "<p>Still fine?</p><p>Yes</p>");
+    assert.equal(p[0].innerHTML, "Still fine?");
+
+    var p2 = element .getElementsByTagName("p");
+    assert.equal(p2[1].innerHTML, "Yes");
+});
+
+QUnit.test("Idom patch tokens", function(assert) {
+    var element = document.getElementById("test-2");
+    idom.patchInner(element, [
+        {type: "open", tag: "p", attributes:[]},
+        {type: "text", data: "Hello World!"},
+        {type: "close", tag: "p"}
+    ]);
+
+    var p = element .getElementsByTagName("p");
+    assert.equal(p[0].innerHTML, "Hello World!");
+
+    /* The p element we retrieved earlier should still be usable */
+    idom.patchInner(element, [
+        {type: "open", tag: "p", attributes:[]},
+        {type: "text", data: "How are you doing"},
+        {type: "close", tag: "p"}
+    ]);
+    assert.equal(p[0].innerHTML, "How are you doing");
+
+    /* Adding an element */
+    idom.patchInner(element, [
+        {type: "open", tag: "p", attributes:[]},
+        {type: "text", data: "Still fine?"},
+        {type: "close", tag: "p"},
+
+        {type: "open", tag: "p", attributes:[]},
+        {type: "text", data: "Yes"},
+        {type: "close", tag: "p"}
+    ]);
     assert.equal(p[0].innerHTML, "Still fine?");
 
     var p2 = element .getElementsByTagName("p");
