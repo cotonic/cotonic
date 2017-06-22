e41b8d62560ff81bc3fac81ade7405ce1589d2c9
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2017-06-22 18:56:11 +0200

    Fix: Added missing {

--- src/cotonic.tokenizer.js
+++ src/cotonic.tokenizer.js
@@ -930,7 +930,7 @@ var cotonic = cotonic || {};
             element.innerHTML = "";
 
             /* Array.from is not always available on IE */
-            if(d.split(/(?=(?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/).length != 1) 
+            if(d.split(/(?=(?:[\0-\t\x0B\f\x0E-\u2027\u202A-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/).length != 1) {
                 return undefined;
             }
 
