ae8c07f07d96344d405c1ce042bfac3218cdd894
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2018-01-27 18:03:11 +0100

    WIP: Adding worker - page postMessage protocol.

--- src/cotonic.worker.js
+++ src/cotonic.worker.js
@@ -14,16 +14,156 @@
  * limitations under the License.
  */
 
+ /**
+  * Description of messages
+
+  topic:
+
+   message-cmd:
+       CONNECT, CONNACK,
+
+       PUBLISH,
+       PUBACK,
+       PUBREC,
+       PUBREL,
+       PUBCOMP,
+
+       SUBSCRIBE,
+       PUBCOMP,
+       SUBSCRIBE,
+       SUBACK,
+       UNSUBSCRIBE,
+       UNSUBACK,
+
+       PINGREQ,
+       PINGRESP,
+       DISCONNECT
+
+   message:
+   {cmd:<message-cmd>,
+      payload: <payload>
+
+   connect-payload:
+       {client_identifier: <id>,
+            will_topic: <topic>,
+            will_message: <payload>}
+  */
+
 "use strict";
 
 /* Cotonic worker code */
 
 (function(self) {
+    /* disconnected, connecting, connected */
+    var sub_id = 0;
+    var subscriptions = {}; /* sub_id -> callback */
+
+    /**
+     * Worker states
+     */
+
+    /* The worker is not connected to the page */
+    var disconnected = {
+        onMessage: function(e) {},
+        onError: function(e) {}
+    }
+
+    /* The worker send a connect message and is waiting for response */
+    var connecting = {
+        onMessage: function(e) {
+            var cmd = e.data.cmd;
+
+            if(cmd != "connack") {
+                self.state = disconnected;
+            }
+
+            if(self.on_connect) self.on_connec();
+        }
+    }
+
+    /* The worker is connected, we can subscribe and receive messages */
+    var connected = {
+        onMessage: function(e) {
+            var cmd = e.data.cmd;
+
+            if (cmd == "publish") {
+                var sub_info = subscriptions[e.data.id];
+                sub_info.callback(e.data.topic, e.data.payload);
+                return;
+            }
+
+            if(cmd == "suback") {
+                var sub_info = subscriptions[e.data.id];
+                if(sub_info.sub_ack) {
+                    sub_info.sub_ack();
+                    sub_info.sub_ack = undefined;
+                }
+                return;
+            }
+
+            if(cmd == "disconnect") {
+            }
+
+        }
+    }
+
+    var state; /* disconnected, connecting, connected */
+
+    function init(endpoint) {
+        self.state = disconnected;
+
+        endpoint.addEventListener("message", self.state.onMessage);
+        endpoint.addEventListener("error", self.state.onError);
+    }
+
+    function _onError() {
+    }
+
     var selfClose = self.close;
 
     self.close = function() {
-        self.postMessage("closed");
+        self.disconnect();
         selfClose();
     }
 
+    self.connect = function(id, willTopic, willMessage) {
+        if(state === disconnected) {
+
+            /* */
+            self.state = connecting;
+            self.postMessage({cmd: "connect", id: id, willTopic: willTopic, willMessage: willMessage});
+
+            /* connect timeout */
+        } else {
+            // wrong state
+        }
+    }
+
+    self.is_connected = function() {
+        return self.state === connected;
+    }
+
+    self.disconnect = function() {
+        if(self.state !== connected) return;
+    }
+
+    self.publish = function(topic, payload) {
+        if(!self.is_connected()) throw "not connected"
+
+        self.postMessage({cmd: "publish", topic: topic, payload: payload});
+    }
+
+    self.subscribe = function(topic, callback, sub_ack) {
+        if(!self.is_connected()) throw "not connected"
+
+        var id = sub_id++;
+        subscriptions[id] = {callback: callback, sub_ack: sub_ack};
+
+        self.postMessage({cmd: "subscribe", topic: topic, id: id});
+    }
+
+    self.unsubscribe = function(topic, callback) {
+    }
+
+    init(self);
 })(self);
--- test/test-worker.js
+++ test/test-worker.js
@@ -1,8 +1,23 @@
 /**
- * Test worker. 
+ * Test worker.
  */
+
 "use strict";
 
 importScripts("../src/cotonic.worker.js");
 
 console.log("worker here");
+
+self.on_connect = function() {
+    console.log("connected")
+
+    var foo_id = self.subscribe("~pagesession/foo", function(topic, payload) {
+        console.log("got foo: " + payload);
+    })
+
+    var bar_id = self.subscribe("~pagesession/bar", function(topic, payload) {
+        console.log("got bar: " + payload);
+    })
+}
+
+self.connect("test-worker");
