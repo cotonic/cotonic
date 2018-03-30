d247b5a4022eb6a20aacb35823dcc455f8f2c145
Marc Worrell <marc@worrell.nl>
2018-03-30 21:42:30 +0200

    Start with bridge code.

--- src/cotonic.mqtt_bridge.js
+++ src/cotonic.mqtt_bridge.js
@@ -19,7 +19,90 @@ var cotonic = cotonic || {};
 
 (function (cotonic) {
 
+    // Bridges to remote servers and clients
+    var bridges = {};
+
+    var newBridge = function( remote ) {
+        remote = remote || 'origin';
+        if (sessions[ remote ]) {
+            return bridges[remote];
+        } else {
+            var bridge = new mqttBridge();
+            bridges[remote] = bridge;
+            bridge.connect(remote);
+            return bridge;
+        }
+    };
+
+    var findBridge = function( remote ) {
+        remote = remote || 'origin';
+        return bridges[remote];
+    };
+
+
+    /*************************************************************************************************/
+    /****************************************** MQTT Bridge ******************************************/
+    /*************************************************************************************************/
+
+    function mqttBridge () {
+
+        var remote;
+        var session;
+        var self = this;
+        var clientId;
+        var routingId;
+
+        // TODO: pass authentication details to the session
+        this.connect = function ( remote ) {
+            self.remote = remote;
+            // 1. Start a mqtt_session for the remote
+            self.session = cotonic.mqtt_session.newSession(remote, self);
+        }
+
+        this.match = function ( topicId ) {
+            return self.clientId === topicId || self.routingId === topicId;
+        }
+
+        this.auth = function ( /* ... */ ) {
+            // Start a re-authentication, useful when logging on
+            // TODO: decide if we need to restart the session with a new clientId (could be an option)
+        }
+
+        this.sessionConnack = function ( clientId, connack ) {
+            // 1. Register the clientId and the optional 'cotonic-routing-id' property
+            // 2. Check 'session_present' flag
+            //    - If not present then:
+            //      1. Add subscription on server for "bridge/<clientId>/#"
+            //      2. Add subscription on server for "bridge/<routingId>/#"
+            //      3. Resubscribe client subscriptions (fetch all local subscriptions matching 'bridge/<remote>/#')
+            // Publish to '$bridge/<remote>/status' topic that we connected (retained)
+            // Subscribers then handle the 'session_present' flag.
+        }
+
+        this.sessionAuth = function ( ConnectOrAuth, isConnected ) {
+            // Either:
+            // - Add authentication credentials to the CONNECT packet
+            // - Received an AUTH packet, perform extended (re-)authentication
+        }
+
+        this.sessionDisconnect = function ( optDisconnect ) {
+            // Publish to '$bridge/<remote>/status' topic that this remote disconnected (retained)
+        }
+
+    }
+
+
+    function init() {
+        // 1. Subscribe to 'bridge/#'
+        // 2. Add a mqtt router hook for subscriptions to 'bridge/#'
+        // 3. ?? Start a bridge to 'origin'
+    }
+
+    init();
+
+    // Publish the MQTT bridge functions.
     cotonic.mqtt_bridge = cotonic.mqtt_bridge || {};
-    // cotonic.mqtt_bridge.newSession = newSession;
+    cotonic.mqtt_bridge.newBridge = newBridge;
+    cotonic.mqtt_bridge.findBridge = findBridge;
 
 }(cotonic));
