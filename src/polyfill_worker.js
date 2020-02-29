400a11d208e733de141fb430a0dc13158ae65267
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2020-02-29 21:11:04 +0100

    All tests now work

--- src/polyfill_worker.js
+++ src/polyfill_worker.js
@@ -1,8 +1,7 @@
 
 (function(global) {
     if(!(global.Promise && global.fetch && String.prototype.startsWith && String.prototype.codePointAt)) {
-        const src = https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith%2CString.prototype.codePointAt%2cPromise
-
+        const src = "https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith%2CString.prototype.codePointAt%2cPromise%2cObject.assign"
         importScripts(src);
     } 
 }(this));
