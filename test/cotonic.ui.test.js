db09216a4d51ed1072faa309a17263d6ca541cb9
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2021-12-05 22:43:31 +0100

    Added tests for adding and removing state-ui data state and classes

--- test/cotonic.ui.test.js
+++ test/cotonic.ui.test.js
@@ -27,5 +27,44 @@ QUnit.test("Basic render test", function(assert) {
         ui.render();
     }, 1000);
 
-    assert.equal(true, true);
+    assert.ok(true);
+});
+
+QUnit.test("State data management test", function(assert) {
+    const html = document.body.parentElement;
+
+    ui.updateStateData("testing", {yes: "yes", no: "no"});
+    assert.equal(html.dataset.uiStateTestingYes, "yes")
+    assert.equal(html.dataset.uiStateTestingNo, "no")
+
+    ui.updateStateData("testing", {yes: "yes", no: "no", onetwothree: 123});
+    assert.equal(html.dataset.uiStateTestingYes, "yes");
+    assert.equal(html.dataset.uiStateTestingNo, "no");
+    assert.equal(html.dataset.uiStateTestingOnetwothree, "123");
+
+    ui.updateStateData("testing", {yes: "yes"});
+    assert.equal(html.dataset.uiStateTestingYes, "yes");
+    assert.equal(html.dataset.uiStateTestingNo, undefined);
+    assert.equal(html.dataset.uiStateTestingOnetwothree, undefined);
+
+    ui.updateStateData("testing", {});
+    assert.equal(html.dataset.uiStateTestingYes, undefined);
+});
+
+QUnit.test("State class test", function(assert) {
+    const html = document.body.parentElement;
+
+    ui.updateStateClass("testing", ["xyz", "abc"] );
+    assert.ok(html.classList.contains("ui-state-testing-xyz"));
+    assert.ok(html.classList.contains("ui-state-testing-abc"));
+
+    ui.updateStateClass("testing", ["xyz"] );
+    assert.ok(html.classList.contains("ui-state-testing-xyz"));
+    assert.notOk(html.classList.contains("ui-state-testing-abc"));
+
+    ui.updateStateClass("testing", [] );
+    assert.notOk(html.classList.contains("ui-state-testing-xyz"));
+    assert.notOk(html.classList.contains("ui-state-testing-abc"));
+
+    assert.ok(true);
 });
