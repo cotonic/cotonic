4b44dafdcfa4b50666b8edc3211266dadfc4678f
williamthome <willegp88@gmail.com>
2022-05-19 09:45:57 -0300

    Fix python version compatibility

--- devserver.py
+++ devserver.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
 import sys
-import BaseHTTPServer
-import SimpleHTTPServer
+if sys.version_info.major < 3:
+    import BaseHTTPServer
+    import SimpleHTTPServer
+    baseServer = BaseHTTPServer
+    simpleServer = SimpleHTTPServer
+else:
+    import http.server
+    baseServer = http.server
+    simpleServer = http.server
 
-class NoCacheHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class NoCacheHandler(simpleServer.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate, post-check=0, pre-check=0")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
 
-        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        simpleServer.SimpleHTTPRequestHandler.end_headers(self)
 
 def serve(address):
-    httpd = BaseHTTPServer.HTTPServer(address, NoCacheHandler)
+    httpd = baseServer.HTTPServer(address, NoCacheHandler)
     sa = httpd.socket.getsockname()
     print("Serving HTTP on", sa[0], "port", sa[1], "...")
     httpd.serve_forever()
