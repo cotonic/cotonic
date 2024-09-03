5bdf68b74e8cdce66c8c61c29eee7f4fc6cfcf3a
Marc Worrell <marc@worrell.nl>
2024-09-03 14:36:22 +0200

    Release 1.7.0 (#109)

--- src/cotonic.js
+++ src/cotonic.js
@@ -15,7 +15,7 @@
  */
 
 /* Current cotonic version */
-const VERSION = "1.6.2";
+const VERSION = "1.7.0";
 
 /* Get a configuration from a global cotonic variable, when available */
 const config = (globalThis.cotonic && globalThis.cotonic.config) ? globalThis.cotonic.config : {};
